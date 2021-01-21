71c7f2a5b26ce9b991d6672c6534c980778afc51
AbelLlontopMeza <a.llontopmeza@gmail.com>
2021-01-21 12:45:50 -0500

    modoOscuroEmpelados

--- src/config/config.php
+++ src/config/config.php
@@ -1,12 +1,17 @@
 <?php
-$estadoAplicacion = "desarrollo";
+$estadoAplicacion = "produccion";
 
 if($estadoAplicacion=="desarrollo"){
-	define('URL','http://localhost/SI/');
-	define('HOST','localhost');
-	define('DB','si');
-	define('USER','root');
-	define('PASSWORD','');
+	define('URL','http://localhost/GITHUB/Producto-Final-Sistemas-de-Informacion-II/');
+	//define('HOST','localhost');
+	//define('DB','si');
+	//define('USER','root');
+	//define('PASSWORD','');
+	//define('CHARSET','utf8mb4');
+	define('HOST','bagx6dzg1mqq2qw80uuk-mysql.services.clever-cloud.com');
+	define('DB','bagx6dzg1mqq2qw80uuk');
+	define('USER','uvxcpgaty7nu2bpb');
+	define('PASSWORD','qVUkm7m9ys1DnzuZjTcb');
 	define('CHARSET','utf8mb4');
 }else{
 	define('URL','https://store-k.herokuapp.com/');
