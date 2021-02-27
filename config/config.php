c5c52c01335c71e2f02a0094d04570eb539a2ad5
AbelLlontopMeza <a.llontopmeza@gmail.com>
2021-02-27 11:36:02 -0500

    correcion de rutas config

--- config/config.php
+++ config/config.php
@@ -3,18 +3,18 @@
 $estadoAplicacion = "desarrollo";
 
 if ($estadoAplicacion == "desarrollo") {
-    define('URL', 'http://localhost/GITHUB/storek/');
+    define('URL', 'http://localhost/GITHUB/Producto-Final-Sistemas-de-Informacion-II/');
     define('HOST','localhost');
     define('DB','storek');
     define('USER','root');
     define('PASSWORD','');
     define('CHARSET','utf8mb4');
 
-    define('RUTA_DOMINIO',$_SERVER['DOCUMENT_ROOT'].'/GITHUB/storek/dominio/');
-    define('RUTA_LOGICA',$_SERVER['DOCUMENT_ROOT'].'/GITHUB/storek/dominio/logica/');
-    define('RUTA_PERSISTENCIA',$_SERVER['DOCUMENT_ROOT'].'/GITHUB/storek/persistencia/');
-    define('RUTA_PRESENTACION',$_SERVER['DOCUMENT_ROOT'].'/GITHUB/storek/presentacion/');
-    define('RUTA_HELPER',$_SERVER['DOCUMENT_ROOT'].'/GITHUB/storek/helpers/');
+    define('RUTA_DOMINIO',$_SERVER['DOCUMENT_ROOT'].'/GITHUB/Producto-Final-Sistemas-de-Informacion-II/dominio/');
+    define('RUTA_LOGICA',$_SERVER['DOCUMENT_ROOT'].'/GITHUB/Producto-Final-Sistemas-de-Informacion-II/dominio/logica/');
+    define('RUTA_PERSISTENCIA',$_SERVER['DOCUMENT_ROOT'].'/GITHUB/Producto-Final-Sistemas-de-Informacion-II/persistencia/');
+    define('RUTA_PRESENTACION',$_SERVER['DOCUMENT_ROOT'].'/GITHUB/Producto-Final-Sistemas-de-Informacion-II/presentacion/');
+    define('RUTA_HELPER',$_SERVER['DOCUMENT_ROOT'].'/GITHUB/Producto-Final-Sistemas-de-Informacion-II/helpers/');
 
 } else {
     define('URL', 'https://store-k.herokuapp.com/');
